72532f8820b2fe38ef5b108345143fe825dc7359
Benjamin Kim <166325010+BenK03@users.noreply.github.com>
2025-12-27 19:30:32 -0500

    Update content.js

--- src/content/content.js
+++ src/content/content.js
@@ -6,19 +6,38 @@ function getPageKey() {
 
 // Read saved page data from the chrome storage
 function readPageData(pageKey, callback) {
+    // get the pageKey data and store it in storedData
+    chrome.storage.local.get(pageKey, function (result) {
+        var storedData = result[pageKey];
 
+        // if nothing 
+        if (storedData === undefined) {
+        callback({});
+
+        // if data exists it gives us the data
+        } else {
+        callback(storedData);
+        }
+    });
 }
 
 // Saves updated pin data to chrome storage
-function writePageData(pageKey, Data, callback) {
+function writePageData(pageKey, data, callback) { // pageKey: where to store, data: what to store, callback: code to run after
+    var objToSave = {}; // create an empty obj
+    objToSave[pageKey] = data;
 
+    // saves obj to chrome storage
+    chrome.storage.local.set(objToSave, function () {
+        callback();
+    });
 }
 
 // get current scroll position (returns vertical scroll position)
 function getCurrentScrollPosition() {
-
+    return window.scrollY;
 }
 
 function savePin(slotNumber, name) {
+    var pageKey = getPageKey();
 
 }
